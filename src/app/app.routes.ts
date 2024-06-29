b3e57c25289c30533defb2b90eed22c6543541f9
CHRIS8530 <Maelradec85@proton.me>
2024-06-29 18:26:19 +0200

    routes.ts component routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { ShowMovieComponent } from './pages/show-movie/show-movie.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full',
+      },
+      {
+        path: 'movies',
+        component: MoviesComponent,
+      },
+      {
+        path: 'show-movie/:movieId',
+        component: ShowMovieComponent,
+      },
+];
