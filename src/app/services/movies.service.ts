d8d61f13f481a494815238e271e35ed697658df3
CHRIS8530 <Maelradec85@proton.me>
2024-07-01 21:08:33 +0200

    define base URL for movie images & Initia. var +injecting HTTPclient

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -6,10 +6,15 @@ import { map } from 'rxjs';
 import { Videos } from '../models/video';
 import { Credits } from '../models/credit';
 
+export const imagesBaseUrl = 'https://image.tmdb.org/t/p/';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
+  private apiUrl = 'https://api.themoviedb.org/3';
+  private apiKey = environment.apiKEY;
+  private httpClient = inject(HttpClient);
   constructor() { }
 }
